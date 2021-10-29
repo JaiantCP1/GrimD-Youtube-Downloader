e4ec175b1c8bf2a785c384838465b338035fd12c
Jean Carlo <66201344+JaintC@users.noreply.github.com>
2021-10-29 11:11:01 -0300

    Create all_formats_list.py

---
+++ GrimD 1.4.0/all_formats_list.py
@@ -0,0 +1,34 @@
+list_formats = [ 
+        "No conversion",
+         
+         "mp3-128kbps",
+         "mp3-192kbps",
+         "mp3-256kbps",
+         "mp3-320kbps",
+          
+          "mp4-144p",
+          "mp4-240p",
+          "mp4-360p",
+          "mp4-480p",
+          "mp4-720p",
+          "mp4-1080p",
+          "mp4-1440p",
+          "mp4-2160p",
+
+          "mkv-144p",
+          "mkv-240p",
+          "mkv-360p",
+          "mkv-480p",
+          "mkv-720p",
+          "mkv-1080p",
+          "mkv-1440p",
+          "mkv-2160p",
+
+          "mpeg-144p",
+          "mpeg-240p",
+          "mpeg-360p",
+          "mpeg-480p",
+          "mpeg-720p",
+          "mpeg-1080p",
+        ]
+    
